c0ff27843869b85b9a05339e2b88038b0f7211c2
mrwebwork <tech@allanswebwork.info>
2023-07-06 11:29:03 -0500

    Complete AuthUI and sign in options

--- app/(site)/components/AuthSocialButton.tsx
+++ app/(site)/components/AuthSocialButton.tsx
@@ -1,8 +1,36 @@
-const AuthSocialButton = () => {
+import { IconType } from "react-icons";
+
+interface AuthSocialButtonProps {
+  icon: IconType;
+  onClick: () => void;
+}
+
+const AuthSocialButton: React.FC<AuthSocialButtonProps> = ({
+  icon: Icon,
+  onClick,
+}) => {
   return (
-    <div>
-      <h1>AuthSocialButton</h1>
-    </div>
+    <button
+      type="button"
+      onClick={onClick}
+      className="
+    inline-flex
+    w-full
+    justify-center
+    bg-white
+    px-4
+    py-2
+    text-gray-500
+    shadow-sm
+    ring-1
+    ring-inset
+    ring-gray-300
+    hover:bg-gray-50
+    focus:outline-offset-0
+    "
+    >
+      <Icon />
+    </button>
   );
 };
 
