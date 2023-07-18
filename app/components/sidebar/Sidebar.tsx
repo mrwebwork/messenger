d2e6e31d887b338243ad76282187eaba092e8277
mrwebwork <tech@allanswebwork.info>
2023-07-18 14:01:37 -0500

    Created MobileItem and MobileFooter

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,8 +1,11 @@
 import DesktopSidebar from "./DesktopSidebar";
+import MobileFooter from "./MobileFooter";
+
 async function Sidebar({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-full">
       <DesktopSidebar />
+      <MobileFooter />
       <main className="h-full lg:pl-20">{children}</main>
     </div>
   );
