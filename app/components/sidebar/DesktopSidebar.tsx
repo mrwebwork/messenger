58c1523322b02c10a01d121b67213f5da46b0459
mrwebwork <tech@allanswebwork.info>
2023-07-19 08:52:48 -0500

    Set up images for auth users, add objects to next.config, create getCurrentUser.ts file

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -6,9 +6,18 @@ import DesktopItem from "./DesktopItem";
 
 import useRoutes from "@/app/hooks/useRoutes";
 
-const DesktopSidebar = () => {
+import { User } from "@prisma/client";
+import Avatar from "../Avatar";
+
+interface DesktopSidebarProps {
+  currentUser: User;
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
   const routes = useRoutes();
   const [isOpen, setIsOpen] = useState(false);
+
+  console.log(currentUser);
   return (
     <div
       className="
@@ -41,6 +50,28 @@ const DesktopSidebar = () => {
             />
           ))}
         </ul>
+      </nav>
+      <nav
+        className="
+    mt-4
+    flex
+    flex-col
+    justify-between
+    items-center
+      "
+      >
+        <div
+          onClick={() => {
+            setIsOpen;
+          }}
+          className="
+          cursor-pointer
+          hover:opacity-75
+          transition
+          "
+        >
+          <Avatar user={currentUser} />
+        </div>
       </nav>
     </div>
   );
