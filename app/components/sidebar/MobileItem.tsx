ce734105cab7f706bd3b56546b366857e6518df9
mrwebwork <tech@allanswebwork.info>
2023-07-19 08:23:39 -0500

    Style mobile and desktop sidebar

--- app/components/sidebar/MobileItem.tsx
+++ app/components/sidebar/MobileItem.tsx
@@ -21,8 +21,28 @@ const MobileItem: React.FC<MobileItemProps> = ({
   };
 
   return (
-    <Link href={href}>
-      <Icon />
+    <Link
+      onClick={onClick}
+      href={href}
+      className={clsx(
+        `
+    group
+    flex
+    gap-x-3
+    text-sm
+    leading-6
+    font-semibold
+    w-full
+    justify-center
+    p-4
+    text-gray-500
+    hover:text-black
+    hover:bg-gray-100
+    `,
+        active && "bg-gray-100 text-black"
+      )}
+    >
+      <Icon className="h-6 w-6" />
     </Link>
   );
 };
