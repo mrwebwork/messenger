991f3813b558c7bb6bd015684a7356eaf70a5395
mrwebwork <tech@allanswebwork.info>
2023-07-19 08:52:48 -0500

    Set up images for auth users, add objects to next.config, create getCurrentUser.ts file

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,10 +1,12 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
 import DesktopSidebar from "./DesktopSidebar";
 import MobileFooter from "./MobileFooter";
 
 async function Sidebar({ children }: { children: React.ReactNode }) {
+  const currentUser = await getCurrentUser();
   return (
     <div className="h-full">
-      <DesktopSidebar />
+      <DesktopSidebar currentUser={currentUser!} />
       <MobileFooter />
       <main className="h-full lg:pl-20">{children}</main>
     </div>
