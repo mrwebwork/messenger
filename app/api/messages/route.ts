04b0ddb16b5daa8094c89f039ec9b486bfed0f59
mrwebwork <tech@allanswebwork.info>
2023-07-22 02:29:02 -0500

    Send message from UI to prisma, clean up IF statements

---
+++ app/api/messages/route.ts
@@ -0,0 +1,69 @@
+import { NextResponse } from "next/server";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from "@/app/libs/prismadb";
+
+export async function POST(request: Request) {
+  try {
+    const currentUser = await getCurrentUser();
+    const body = await request.json();
+    const { message, image, conversationId } = body;
+
+    if (!currentUser?.id || !currentUser?.email) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const newMessage = await prisma.message.create({
+      data: {
+        body: message,
+        image: image,
+        conversation: {
+          connect: {
+            id: conversationId,
+          },
+        },
+        sender: {
+          connect: {
+            id: currentUser.id,
+          },
+        },
+        seen: {
+          connect: {
+            id: currentUser.id,
+          },
+        },
+      },
+      include: {
+        seen: true,
+        sender: true,
+      },
+    });
+
+    const updatedConversation = await prisma.conversation.update({
+      where: {
+        id: conversationId,
+      },
+      data: {
+        lastMessageAt: new Date(),
+        messages: {
+          connect: {
+            id: newMessage.id,
+          },
+        },
+      },
+      include: {
+        users: true,
+        messages: {
+          include: {
+            seen: true,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(newMessage);
+  } catch (error: any) {
+    //! Console Log Error
+    console.log(error, "Error Messages");
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
