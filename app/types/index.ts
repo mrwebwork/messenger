ba4e1f547529484a69a876c0ad1f7578986a58cc
mrwebwork <tech@allanswebwork.info>
2023-07-22 00:46:19 -0500

    Define FullMessageType and FullConversationType with associated relationships

---
+++ app/types/index.ts
@@ -0,0 +1,12 @@
+
+import { Conversation, Message, User } from "@prisma/client";
+
+export type FullMessageType = Message & {
+    sender: User;
+    seen: User[]
+};
+
+export type FullConversationType = Conversation & {
+    users: User[];
+    messages: FullMessageType[];
+}
