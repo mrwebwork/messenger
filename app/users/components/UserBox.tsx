8d7473b0f2303b0b92e07030a7c754b27843a775
mrwebwork <tech@allanswebwork.info>
2023-07-22 08:43:44 -0500

    Cleaned up UserBox.tsx file

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -35,17 +35,17 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
       <div
         onClick={handleUserClick}
         className="
-    w-full
-    relative
-    flex
-    items-center
-    space-x-3
-    bg-white
-    p-3
-    hover:bg-neutral-100
-    rounded-lg
-    transition
-    cursor-pointer
+            w-full
+            relative
+            flex
+            items-center
+            space-x-3
+            bg-white
+            p-3
+            hover:bg-neutral-100
+            rounded-lg
+            transition
+            cursor-pointer
     "
       >
         <Avatar user={data} />
